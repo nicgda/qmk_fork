b6d60929069b3d6d5daf269c6307d37dcdfd6e15
nic <nicgda@nic.ngda.ca>
2023-03-20 23:58:51 -0400

    Jris65 CE stuff

---
+++ keyboards/jris_ce/jris65/jris65.c
@@ -0,0 +1,30 @@
+/*
+Copyright 2023 QMK jris community
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*******************************************************************
+ * ,--.   ,--.  ,---.  ,------. ,--.  ,--.,--.,--.  ,--. ,----.    *
+ * |  |   |  | /  O  \ |  .--. '|  ,'.|  ||  ||  ,'.|  |'  .-./    *
+ * |  |.'.|  ||  .-.  ||  '--'.'|  |' '  ||  ||  |' '  ||  | .---. *
+ * |   ,'.   ||  | |  ||  |\  \ |  | `   ||  ||  | `   |'  '--'  | *
+ * '--'   '--'`--' `--'`--' '--'`--'  `--'`--'`--'  `--' `------'  *
+ *                                                                 *
+ * This code was not provided, supported nor endorsed by Irislab.  *
+ * This is a community project to let jris65 owner easily use QMK  *
+ * on their keyboards, as intended when they bought it.            *
+ *******************************************************************/
+
+#include "jris65.h"
