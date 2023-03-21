216d2fa8b860e6ebf8cd38207246170fe2a20921
nic <nicgda@nic.ngda.ca>
2023-03-20 23:58:51 -0400

    Jris65 CE stuff

---
+++ keyboards/jris_ce/jris65/keymaps/ansi_split_bs_lshift/keymap.c
@@ -0,0 +1,38 @@
+/*******************************************************************
+ * ,--.   ,--.  ,---.  ,------. ,--.  ,--.,--.,--.  ,--. ,----.    *
+ * |  |   |  | /  O  \ |  .--. '|  ,'.|  ||  ||  ,'.|  |'  .-./    *
+ * |  |.'.|  ||  .-.  ||  '--'.'|  |' '  ||  ||  |' '  ||  | .---. *
+ * |   ,'.   ||  | |  ||  |\  \ |  | `   ||  ||  | `   |'  '--'  | *
+ * '--'   '--'`--' `--'`--' '--'`--'  `--'`--'`--'  `--' `------'  *
+ *                                                                 *
+ * This code was not provided, supported nor endorsed by Irislab.  *
+ * This is a community project to let jris65 owner easily use QMK  *
+ * on their keyboards, as intended when they bought it.            *
+ *******************************************************************/
+#include QMK_KEYBOARD_H
+
+/* Keymap
+        Esc      1        2        3        4        5        6        7        8        9        0         -       =        BackSpc  Del     PrntScr
+        Tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \        PgUp
+        Caps     A        S        D        F        G        H        J        K        L        ;        '                 Enter    PgDn
+        Sh_L     MO(1)    Z        X        C        V        B        N        M        ,        .        /        Sh_R     Up       MO(1)
+        Ct_L     GUI_L    Alt_L                      SPACE                               Atl_R    Ct_R              Left     Down     Right
+*/
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [0] = LAYOUT_65_ansi_split_bs_lshift(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  KC_PSCR,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,
+        KC_LSFT, MO(1),   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   MO(1),
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+    [1] = LAYOUT_65_ansi_split_bs_lshift(
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, QK_BOOT,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_END,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______,                            _______,                   _______, _______,          _______, _______, _______
+    ),
+};
